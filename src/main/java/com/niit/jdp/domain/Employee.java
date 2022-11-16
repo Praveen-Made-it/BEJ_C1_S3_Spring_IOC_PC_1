7c5da39bdf7d29102af8b531ed8bb8934732f2b2
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 17:09:12 +0530

    created the field for Department class with Autowired annotation inside the Employee class

--- src/main/java/com/niit/jdp/domain/Employee.java
+++ src/main/java/com/niit/jdp/domain/Employee.java
@@ -7,9 +7,14 @@
 
 package com.niit.jdp.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Employee {
     private int employeeId;
     private String empoyeeName;
     private String employeeSalary;
 
+
+    @Autowired
+    private Department department;
 }
