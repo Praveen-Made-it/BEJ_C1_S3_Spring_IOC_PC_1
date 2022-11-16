f09e6a0d3351b267680ad126f75663f67edeedd3
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 17:05:25 +0530

    created getter and setter methods for Department class

--- src/main/java/com/niit/jdp/domain/Department.java
+++ src/main/java/com/niit/jdp/domain/Department.java
@@ -18,4 +18,20 @@ public class Department {
         this.departmentId = departmentId;
         this.departmentName = departmentName;
     }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
