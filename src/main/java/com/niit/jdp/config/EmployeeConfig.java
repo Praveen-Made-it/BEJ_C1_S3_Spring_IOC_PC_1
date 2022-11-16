50624e66c8a7f963b3cb453a56534223372fdd85
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 17:29:31 +0530

    created the getEmployee method to return the employee details and setted the values for employeeId,employeeName,employeeSalary.

--- src/main/java/com/niit/jdp/config/EmployeeConfig.java
+++ src/main/java/com/niit/jdp/config/EmployeeConfig.java
@@ -7,5 +7,20 @@
 
 package com.niit.jdp.config;
 
+import com.niit.jdp.domain.Employee;
+import org.springframework.context.annotation.Bean;
+
 public class EmployeeConfig {
+    @Bean("employee1")
+    // A method which returns an object of type Employee.
+    public Employee getEmployee() {
+        // Creating an object of type Employee.
+        Employee employee = new Employee();
+        // Setting the value of employeeId, employeeName, employeeSalary
+        employee.setEmployeeId(27186);
+        employee.setEmpoyeeName("Praveen");
+        employee.setEmployeeSalary("5_00_000");
+        return employee;
+    }
+
 }
