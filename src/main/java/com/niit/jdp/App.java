337e0a023ac549f94f5695825ec0660a9a40bcd9
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 17:58:11 +0530

    final commit

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -4,9 +4,6 @@ import com.niit.jdp.config.EmployeeConfig;
 import com.niit.jdp.domain.Employee;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         // Creating a Spring container and loading the configuration from the EmployeeConfig class.
